4d8473b474cc6a450c1225781a62bceb14a4e91b
Anas <anuslakhani021@gmail.com>
2022-01-15 20:31:48 +0500

    add

---
+++ app/src/main/java/com/coderlytics/apexblogger/ui/activities/BoardingActivity.java
@@ -0,0 +1,16 @@
+package com.coderlytics.apexblogger.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coderlytics.apexblogger.R;
+
+public class BoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boarding);
+    }
+}
